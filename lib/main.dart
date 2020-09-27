4fc283ee4ccedd8bdd715b40f68772d63f507a46
Eric Wu <eric_wu@ihergo.com>
2020-09-27 17:05:22 +0800

    測驗14

--- lib/main.dart
+++ lib/main.dart
@@ -37,3 +37,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+/**
+ * 測驗14
+ * Q: What's a "Screen" in a Flutter App?
+ * A: A widget which controls the entire screen (or at least makes up the main content of the screen).
+ * A screen is a normal widget but it's loaded such that it fills the entire (or the majority) of the device screen. 
+ * Typically, it also controls the app bar etc.
+ * 
+ * Q: What's true about "Screens" and "normal Widgets"?
+ * A: Both are normal widgets in the end, the only difference is how the widgets are used & which role they play.
+ * There is no technical difference between the different kinds of widgets.
+ * 
+ * 這題重要 Q: What's the difference between push() and pushNamed()?
+ * A: push() navigates to a new screen by creating it "on the fly", pushNamed() can only load screens which were registered in advance.
+ * push() creates the MaterialPageRoute object "on the fly" (i.e. in the place where you call push()) 
+ * whereas pushNamed() only works with pre-registered routes (in the routes table of your MaterialApp widget).
+ * 
+ * 這題重要 Q: What exactly is a "named route"?
+ * A: A route which is registered in the routes table - it receives a "name" (key) with which it can be loaded there.
+ * You register key (name) value (route builder function) pairs in the routes table of your MaterialApp/ CupertinoApp widget. 
+ * These are the "named routes" you can use with pushNamed().
+ * 
+ * Q: What's the "Stack of Pages" (or "Stack of Screens")?
+ * A: It's an important concept in mobile navigation. 
+ * New pages are typically pushed on top of the "Stack of Pages/ Screens". 
+ * The top-most (i.e. latest) page/ screen is the visible screen. 
+ * Popping the latest screen therefore moves back to an older screen.
+ * Screen widgets are managed in a stack and you always see the top-most screen. 
+ * Old screens are also kept around (i.e. not deleted) but currently not visible. 
+ * If you move back, you see the old page again since the newer page got popped off the stack.
+ */
